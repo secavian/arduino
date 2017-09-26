10148fcbbf4db8b0058c42a80fc4c8b826e2cc7d
secavian <secavian@users.noreply.github.com>
2017-09-26 00:26:48 -0400

    Create XBeeRxTest.ino

---
+++ libraries/XBee2/examples/XBeeRxTest/XBeeRxTest.ino
@@ -0,0 +1,51 @@
+//TARGET == TEENSY 3.1/3.2
+
+#include <Led.h>
+#include <XBee2.h>
+
+//uncomment to use software serial instead of hardware serial
+//#include <SoftwareSerial.h>
+//SoftwareSerial Serial1(2,3);
+
+Led led(LED_BUILTIN);
+
+RxPacket rx(&Serial1);
+
+void setup(){
+    Serial.begin(9600);
+    
+    //make sure there is a source sending data and match the baud rates.
+    //don't ask how i know this, just take my word for it
+    Serial1.begin(115200);
+}
+
+void loop(){
+    uint8_t r = rx.Read();
+    switch(r){
+        case API_RXPACKET_READ_SUCCESS:
+            led.Toggle();
+
+            Serial.print("\nOK ");
+            for(uint8_t i = 0; i < rx.PayloadLength; i++){
+                uint8_t b = rx.Payload[i];
+                
+                if(b < 0x10){ Serial.print("0"); }
+                Serial.print(b, HEX);
+                Serial.print(" ");
+            }
+            Serial.println();
+            break;
+            
+        case API_RXPACKET_READ_ERROR:
+            led.Toggle();
+
+            Serial.print("\nERR: ERR CODE: 0x");
+            Serial.println(r, HEX);
+            break;
+    }
+    
+    //do other work while waiting on new data
+    Serial.print(".");
+    delay(25);
+}
+
