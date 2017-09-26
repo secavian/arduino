c8325a0f4075089c0b7432c84cb3769f7a27eb73
secavian <secavian@users.noreply.github.com>
2017-09-25 22:18:40 -0400

    Add files via upload

--- libraries/Button/examples/Button_External_Pullup/Button_External_Pullup.ino
+++ libraries/Button/examples/Button_External_Pullup/Button_External_Pullup.ino
@@ -13,34 +13,22 @@ Note that this sketch uses an external pullup resistor on the switch
 ===============================================
 CIRCUIT
 ===============================================
-Connect 330 Ohm resistor (R1) between ground and cathode of LED
-Connect anode of LED to digital pin 11
- (The cathode is typically the short led on the LED)
- 
-Connect momentary contact switch (SW1) between ground and digital pin 8
-Connect 10K Ohm resistor (pullup) between pin side of SW1 and 5v
-
  GND >------[ R1 ]----[ LED1 +]-------> Digital Pin 11
  
               ___|___
  GND >--------+ SW1 +------+----------> Digital Pin 8
                            |
-                           +--[ R2 ]--> 5v
-    
-    Where:
-        R1   = 330 Ohm resisotr
-        R2   = 10K Ohm resistor
-        SW1  = Momentary Contact Switch
-        LED1 = Red LED
+                           +--[ R2 ]--> Vcc
 *********************/
 
 
 Led led(11);
-Button button(12, false);
+Button button(12, Pullup);
 
-void setup() {
-}
+void setup(){ }
 
 void loop() {
     led.OnWhen(button.Pressed());
 }
+
+
