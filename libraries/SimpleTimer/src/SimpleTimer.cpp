cd4ce291a5603a6a584d575f46bbf2f21ba0839b
secavian <github@secavian.com>
2017-10-30 22:34:55 -0400

    Added timespan support. Fixed bug in remaining() method

--- libraries/SimpleTimer/src/SimpleTimer.cpp
+++ libraries/SimpleTimer/src/SimpleTimer.cpp
@@ -3,6 +3,10 @@
 void SimpleTimer::start(){
     _startTicks = millis();
 }
+
+void SimpleTimer::reset(){
+    start();
+}
     
 void SimpleTimer::start(uint32_t millis){
     start();
@@ -16,10 +20,27 @@ uint32_t SimpleTimer::elapsed(){
 uint32_t SimpleTimer::remaining(){
     uint32_t m = millis();
     
-    if((m - _startTicks) > _countDown){ return 0; }
-    return _countDown - m;
+    if((m - _startTicks) > _countDown){ 
+        return 0;
+    }
+    return _countDown - m + _startTicks;
+}
+
+bool SimpleTimer::countdownComplete(void){
+    return remaining() == 0;
 }
 
 void SimpleTimer::wait(uint32_t waitTicks){
     while((millis() - _startTicks) < waitTicks){}
 }
+
+void SimpleTimer::timeElapsed(TimeSpan &timespan){
+    timespan.fromMillis(elapsed());
+}
+
+void SimpleTimer::timeRemaining(TimeSpan &timespan){
+    timespan.fromMillis(remaining());
+}
+
+
+
